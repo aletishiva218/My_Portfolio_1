c474c1f6d315e422c1abdf7e7fbce71877c12d3f
Shiva Aleti <aletishiva218@gmail.com>
2024-08-08 01:19:03 +0530

    removed upload folder

--- index.js
+++ index.js
@@ -12,15 +12,10 @@ dotenv.config()
 
 const port = process.env.PORT;
 
-const _dirname = path.resolve();
-const uploadFolder = path.join(_dirname,"uploads");
-fs.mkdirSync(uploadFolder,{recursive:true})
-
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(uploadFolder))
 
 app.get("/personal",getData)
 app.patch("/personal",updatedData)
